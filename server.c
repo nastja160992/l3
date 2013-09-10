c2d660c222733704b6f9681cbf8c04c71598d420
nastja160992 <mironovich-anast@mail.ru>
2013-09-10 08:37:22 +0300

    Update server.c

--- server.c
+++ server.c
@@ -9,30 +9,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
-
-void* serve_client(void* connection) {
-    int conn = *((int*) connection);
-    char* buffer;
-    FILE* file;
-    long size;
-
-    file = fopen("transfer", "r");
-    if (!file) {
-        strcpy(buffer, "File not found, sorry.\n");
-    } else {
-        fseek(file, 0L, SEEK_END);
-        size = ftell(file);
-        fseek(file, 0L, SEEK_SET);
-        printf("File size: %ld\n", size);
-        buffer = (char*) malloc(sizeof(char) * size);
-        fread(buffer, sizeof(char), size, file);
-    }
-
-    write(conn, buffer, strlen(buffer));
-    printf("Client served.\n");
-    close(conn);
-}
-
+#include "cs.c"
 
 int main(int argc, char *argv[])
 {
